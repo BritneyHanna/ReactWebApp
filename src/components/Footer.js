4c7af32e582a9679cad631dca8fff6cfe9136d68
unknown <b.ahanna@yahoo.com>
2022-08-24 19:54:29 -0400

    added Navbar and Footer Components

---
+++ src/components/Footer.js
@@ -0,0 +1,76 @@
+import React from "react";
+import "./FooterStyles.css";
+import {
+  FaFacebook,
+  FaLinkedin,
+  FaMailBulk,
+  FaPhone,
+  FaSearchLocation,
+  FaTwitter,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaSearchLocation
+              size={20}
+              style={{ color: "#ffffff", marginRight: "2rem" }}
+            />
+            <div>
+              <p>500 React Ave</p>
+              <h4>Fort Lauderdale, FL</h4>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#ffffff", marginRight: "2rem" }}
+              />{" "}
+              1-800-555-5555
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#ffffff", marginRight: "2rem" }}
+              />{" "}
+              mediatate@getenlighted.com
+            </h4>
+          </div>
+        </div>
+        <div className="right">
+          <h4>About Us</h4>
+          <p>
+            We've been apart of the South Florida community for 25 years and our
+            methods have helped people from all walks of life achieve the mental
+            state necessary for success and happiness.
+          </p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "#ffffff", marginRight: "1rem" }}
+            />
+            <FaTwitter
+              size={30}
+              style={{ color: "#ffffff", marginRight: "1rem" }}
+            />
+          
+              <FaLinkedin
+                size={30}
+                style={{ color: "#ffffff", marginRight: "1rem" }}
+                      />
+                        {/* <a  target="_blank" rel="noreferrer" href="https://www.linkedin.com/"></a> */}
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Video.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './VideoStyles.css';
+import yogaVideo from '../assets/naturevid.mp4';
+
+const Video = () => {
+  return (
+    <div className="hero">
+      <video autoPlay loop muted id="video">
+        <source src={yogaVideo} type="video/mp4" />
+      </video>
+      <div className="content">
+        <h1>Meditation Classes</h1>
+<p> Journey within to find inner peace and purpose</p>
+        <div>
+          <Link to="/training" className="btn">
+            Classes
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            Contact Us 
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Video
