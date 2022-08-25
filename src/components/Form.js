48f3c4b3d2f439f1215d2265851e0fae2f85273e
unknown <b.ahanna@yahoo.com>
2022-08-24 22:47:00 -0400

    added changes for form submission

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,15 +4,16 @@ import "./FormStyles.css";
 const Form = () => {
   return (
     <div className="form">
-      <form>
+      <form method="POST" name="contactform" className="contactForm">
+        <input type="hidden" name="form-name" value="contactForm" />
         <label>Your Name</label>
-        <input type="text"></input>
+        <input type="text" name="name"></input>
         <label>Email</label>
-        <input type="email"></input>
+        <input type="email" name="email"></input>
         <label>Subject</label>
         <input type="text"></input>
         <label>Details</label>
-        <textarea rows="6" placeholder="Type a short message here" />
+        <textarea name="message" rows="6" placeholder="Type a short message here" />
         <button className="btn">Submit</button>
       </form>
     </div>
